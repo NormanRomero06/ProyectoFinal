335c8e1bd631d2dd304989cf674a1842d9fbde25
NormanRomero06 <112509122+NormanRomero06@users.noreply.github.com>
2022-11-17 20:29:28 -0600

    Crud Guardado

--- app/src/main/java/ni/edu/uca/infopegue/CrudCliente.kt
+++ app/src/main/java/ni/edu/uca/infopegue/CrudCliente.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import ni.edu.uca.infopegue.dao.ShareObjectAdp.Companion.preferShared
 import ni.edu.uca.infopegue.databinding.FragmentCrudClienteBinding
 
-class CrudCliente : Fragment(){
+class CrudCliente : Fragment() {
     private lateinit var binding: FragmentCrudClienteBinding
 
 
@@ -23,17 +25,63 @@ class CrudCliente : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentCrudClienteBinding.inflate(layoutInflater)
-        iniciar()
+        Guardar()
         Acciones()
         return binding.root
     }
 
-    private fun iniciar() {
+    private fun Guardar() {
+
+        binding.btnGuardar.setOnClickListener {
+
+
+            if (binding.etPrimerNombre.text.toString().isNotEmpty()) {
+                preferShared.saveFnombre(binding.etPrimerNombre.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Primer Nombre", Toast.LENGTH_SHORT).show()
+            }
+
+            if (binding.etSegundoNombre.text.toString().isNotEmpty()) {
+                preferShared.saveSnombre(binding.etSegundoNombre.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Segundo Nombre", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.etPrimerApellido.text.toString().isNotEmpty()) {
+                preferShared.saveFapellido(binding.etPrimerApellido.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Primer Apellido", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.etSegundoApellido.text.toString().isNotEmpty()) {
+                preferShared.saveSapellido(binding.etSegundoApellido.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Segudo Apellido", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.etEdad.text.toString().isNotEmpty()) {
+                preferShared.saveEdad(binding.etEdad.text.toString().toInt())
+            } else {
+                Toast.makeText(this.context, "Inserte Edad", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.etProfesion.text.toString().isNotEmpty()) {
+                preferShared.saveProfesion(binding.etProfesion.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Profesion", Toast.LENGTH_SHORT).show()
+            }
+            if (binding.etDescripPersonal.text.toString().isNotEmpty()) {
+                preferShared.saveDescripcion(binding.etDescripPersonal.text.toString())
+            } else {
+                Toast.makeText(this.context, "Inserte Una Descripcion", Toast.LENGTH_SHORT).show()
+            }
+
+            Toast.makeText(this.context, "Datos Guardados ", Toast.LENGTH_SHORT).show()
+
+        }
+
 
 
     }
+
+
     private fun Acciones() {
 
         binding.IvPersona.setOnClickListener {
